99bd365cb86e1cd63d6ffde5856d4aa5df2c0a34
Adizmobile <adizmobile.com@gmail.com>
2018-05-17 08:59:14 +0700

    Java Mang 1 Chieu

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//  Xuất ra mảng 1 chiều gồm 5 phần tử : 7,8,6,4,2.
+        int A[] = {7,8,6,4,2};
+        for (int i = 0; i < A.length; i ++){
+            System.out.println(A[i]);
+        }
+    }
+}
